fc1da379bc2f7de655a012bcc159744b104fdbaa
basvdijk <bas@atum.fritz.box>
2012-10-22 11:38:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+jQuery SVG animateTransform fix
+===============================
+
+Currently IE9 does support SVG but does not parse the 
+animateTransform tag. Keith Wood has created a jQuery plugin
+called jQuery SVG which allows you to apply the jQuery animate 
+function on SVG elements (http://keith-wood.name/svg.html). 
+
+jQuery SVG combined with this IEfix plugin allows IE9 to 
+animate the elements with animateTransform of an inline SVG 
+image.
+
+License
+=======
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+Installation
+============
+
+You need:
+[*] http://keith-wood.name/svg.html "Keith Wood jQuery SVG plugin including the animation extension"
+[*] "Our IEfix plugin"
---
+++ jquery.svganim-iefix.js
@@ -0,0 +1,86 @@
+/******************************************************************************
+ *
+ * Created:		20-10-2012
+ * Version:     v0.1.0
+ * Author:		Bas van Dijk <info@oxyva.nl>
+ * Description:	Currently IE9 does support SVG but does not parse the 
+ *              animateTransform tag. Keith Wood has created a jQuery plugin
+ *              called jQuery SVG which allows you to apply the jQuery animate 
+ *              function on SVG elements: (http://keith-wood.name/svg.html). 
+ * 
+ *              jQuery SVG combined with this IEfix plugin allows IE9 to 
+ *              animate the elements with animateTransform of an inline SVG 
+ *              image.
+ *
+ *****************************************************************************/
+
+(function($) {
+
+	function runSVGAnimateTransform() {
+	
+		function timeStringToTime(s) {
+			return parseInt(s.substring(0, s.length -1));
+		}
+	
+		function animate(animateTransformElementObj, origin) {
+				
+			var root = $(animateTransformElementObj);
+		
+			var id = "#" + root.parent().attr("id");
+			
+			var type = root.attr("type");
+			var from = root.attr("from");
+			var to = root.attr("to");
+		
+			var repeat = root.get(0).getAttribute("repeatCount");
+				
+			var dur = root.attr("dur");
+				
+			var duration = timeStringToTime(dur) * 1000;		
+			var animString = type + "(" + to + ")";
+		
+			var _origin;
+
+			if (repeat == "indefinite") {
+			
+				if (typeof(origin) === "undefined") {
+					_origin = $(id).attr("transform");
+
+				} else {
+				
+					_origin = origin;
+					$(id).attr("transform", _origin);
+				}
+						
+				$(id).animate({ svgTransform: animString }, duration, function() { animate(root, _origin); });
+			} else {
+				$(id).animate({ svgTransform: animString }, duration);
+			}
+		
+		}
+	
+		$.each($("animateTransform"), function(index, obj) {
+
+			var begin = $(obj).attr("begin");
+
+			if (begin == "1s") {
+				animate(obj);
+			} else {
+				setTimeout(function() { animate(obj); }, timeStringToTime(begin) * 1000);
+			
+			}
+
+		});
+	
+	};
+
+	$(document).ready(function() {
+		
+		if ( $.browser.msie ) {
+		
+			runSVGAnimateTransform();
+		
+		}
+	});
+
+})(jQuery);
